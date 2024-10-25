0aeafb229027417278ce8a00777e8307cb3e4724
Your Name <you@example.com>
2024-10-25 23:21:50 +0800

    finalk

--- RSBSA/Staff/index.php
+++ RSBSA/Staff/index.php
@@ -127,23 +127,25 @@
 													<path d="M7 6.5c0 .795-.504 1.47-1.215 1.75v.5c0 .552-.448 1-1 1s-1-.448-1-1v-.5A1.75 1.75 0 1 1 7 6.5zM5.285 6h1.43c.157-.252.285-.535.285-.875s-.128-.623-.285-.875h-1.43c.157.252.285.535.285.875s-.128.623-.285.875z"/>
 													<path fill-rule="evenodd" d="M8 6a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H5.5a.5.5 0 0 0 0 1H7v1H5.5a.5.5 0 0 0 0 1H8zm5-3H8.246C8.09 1.667 7.64 1 7 1s-1.09.667-1.246 1.5H3.5C2.672 2.5 2 3.172 2 4v6c0 .828.672 1.5 1.5 1.5h.254c.156.833.606 1.5 1.246 1.5s1.09-.667 1.246-1.5H13c.828 0 1.5-.672 1.5-1.5V4c0-.828-.672-1.5-1.5-1.5zM14 4v6H2V4h12z"/>
 												</svg>
-											</div><!--//icon-holder-->
-										</div><!--//col-->
+											</div>
+										</div>
 										<div class="col-auto">
 											<h4 class="app-card-title">Announcement</h4>
-										</div><!--//col-->
-									</div><!--//row-->
-								</div><!--//app-card-header-->
+										</div>
+									</div>
+								</div>
 								<div class="app-card-body px-4">
-									
-								</div><!--//app-card-body-->
+									<p class="intro mb-3">
+										Use this section to create and post important announcements that will be visible to all team members or clients. Click the button below to open a modal window where you can add the details of the announcement, set its visibility, and publish it instantly.
+									</p>
+								</div>
 								<div class="app-card-footer p-4 mt-auto">
 									<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#announcementModal">
 										Post New Announcement
 									</button>
-								</div><!--//app-card-footer-->
-							</div><!--//app-card-->
-						</div><!--//col-->
+								</div>
+							</div>
+						</div>
 						
 						<div class="col-12 col-lg-4">
 							<div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
@@ -151,34 +153,31 @@
 									<div class="row align-items-center gx-3">
 										<div class="col-auto">
 											<div class="app-icon-holder icon-holder-mono">
-												<!-- Help icon (Headset icon for help) -->
+												<!-- Help icon (Headset icon for support) -->
 												<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16">
 													<path d="M8 1a5 5 0 0 0-5 5v4.5H2V6a6 6 0 1 1 12 0v4.5h-1V6a5 5 0 0 0-5-5z"/>
 													<path d="M11 8a1 1 0 0 1 1-1h2v4a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V8zM5 8a1 1 0 0 0-1-1H2v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V8z"/>
 													<path fill-rule="evenodd" d="M13.5 8.5a.5.5 0 0 1 .5.5v3a2.5 2.5 0 0 1-2.5 2.5H8a.5.5 0 0 1 0-1h3.5A1.5 1.5 0 0 0 13 12V9a.5.5 0 0 1 .5-.5z"/>
 													<path d="M6.5 14a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2h-1a1 1 0 0 1-1-1z"/>
 												</svg>
-											</div><!--//icon-holder-->
-										</div><!--//col-->
+											</div>
+										</div>
 										<div class="col-auto">
-											<h4 class="app-card-title">Need more help?</h4>
-										</div><!--//col-->
-									</div><!--//row-->
-								</div><!--//app-card-header-->
+											<h4 class="app-card-title">Staff Message Center</h4>
+										</div>
+									</div>
+								</div>
 								<div class="app-card-body px-4">
-									<div class="intro mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquet eros vel diam semper mollis.</div>
-									<ul class="list-unstyled">
-										<li><strong>Tel:</strong> 0800 1234 5678</li>
-										<li><strong>Email:</strong> <a href="#">support@website.com</a></li>
-									</ul>
-								</div><!--//app-card-body-->
+									<div class="intro mb-3">
+										Access the live chat platform to check client inquiries and respond to user messages in real-time. Click the button below to view and manage ongoing chats with clients.
+									</div>
+								</div>
 								<div class="app-card-footer p-4 mt-auto">
-									<!-- Button that triggers the modal -->
-								<a class="btn app-btn-secondary" href="#" data-bs-toggle="modal" data-bs-target="#liveChatModal">Start Live Chat</a>
-
-								</div><!--//app-card-footer-->
-							</div><!--//app-card-->
-						</div><!--//col-->
+									<!-- Button for staff to open the live chat in a new tab -->
+									<a class="btn app-btn-secondary" href="https://www.tidio.com/" target="_blank" rel="noopener noreferrer">Open Message Center</a>
+								</div>
+							</div>
+						</div>
 					</div><!--//row-->
 
 					
